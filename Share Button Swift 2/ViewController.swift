67082a80d531acd2bf58c6af01f5c8d77a6d4313
davidcorneto <davidcornelius15@gmail.com>
2018-02-07 12:52:06 +0700

    Initial Commit

---
+++ Share Button Swift 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Share Button Swift 2
+//
+//  Created by Muztreat Indonesia on 07/02/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
